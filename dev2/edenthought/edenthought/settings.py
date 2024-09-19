6729debb1d02dd8f96a3799b99312c8e90ca5b73
Brapido <129673180+Brapido@users.noreply.github.com>
2024-09-19 13:36:07 -0400

    Update settings.py

--- dev2/edenthought/edenthought/settings.py
+++ dev2/edenthought/edenthought/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dev2-9qf6.onrender.com']
 
 # CSRF_TRUSTED_ORIGINS = []
 
